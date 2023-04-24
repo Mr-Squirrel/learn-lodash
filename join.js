ea4ed299e27f6084ca56785277eb58c8887ac64c
Meowmir <disewji@gmail.com>
2023-04-24 10:30:36 +0200

    added js docs

--- join.js
+++ join.js
@@ -1,3 +1,9 @@
+/**
+ * Joins an array with a provided separator, else an empty string.
+ * @param {array} array - The array to join.
+ * @param {string} separator - The string to join with.
+ * @returns {string}
+ */
 export default function join(array, separator) {
   if (Array.isArray(array)) {
     return array.join(separator)
