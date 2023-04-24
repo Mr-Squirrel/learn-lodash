7fcfe0003c059532abba0ef1607dc9c08b741440
Nora <106168179+Meowmir@users.noreply.github.com>
2023-04-24 10:43:13 +0200

    [Chore] cleanup join (#3)
    * added type guard to join
    
    * added js docs

--- join.js
+++ join.js
@@ -1,3 +1,12 @@
+/**
+ * Joins an array with a provided separator, else an empty string.
+ * @param {array} array - The array to join.
+ * @param {string} separator - The string to join with.
+ * @returns {string}
+ */
 export default function join(array, separator) {
-  return array.join(separator)
+  if (Array.isArray(array)) {
+    return array.join(separator)
+  }
+  return ''
 }
