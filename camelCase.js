c15be39eeaaa762070590fc68cd5c112647f2095
Meowmir <disewji@gmail.com>
2023-05-02 20:36:03 +0200

    - fixed uppercase when previous is number - removed if statement to check empty space - lowercase when previous was uppercase

--- camelCase.js
+++ camelCase.js
@@ -3,35 +3,44 @@ import join from './join.js'
 export default function camelCase(str) {
   const parts = [...str].map((char, currentIndex, original) => {
 
-    char.toLowerCase() + currentIndex
+    //char.toLowerCase() + currentIndex
 
-    const previous = original[currentIndex-1] || ''
+    const previous = original[currentIndex - 1] || ''
+
+    // I GET THE RIGHT RESULT HERE, WHYYYY
+    console.log(previous)
 
     if (previous.trim() === '') {
       return currentIndex === 0 ? char.toLowerCase() : char.toUpperCase()
     }
 
+    if (previous == Number(previous)) {
+      return char.toUpperCase()
+    }
+
+    if (previous === previous.toUpperCase()) {
+      return char.toLowerCase()
+    }
+    /*
+    Number(”6”) == ”6” // true
+    Number(”6”) === ”6” // false
+    Number(”c”) // NaN
+    */
+
     return char
   })
 
-  return join(parts, '')
+  const noEmptyStrings = parts.filter((str) => str !== ' ')
+  console.log(noEmptyStrings)
+
+  return join(noEmptyStrings, '')
 
   //const noEmptyStrings = parts.filter((str) => str !== ' ')
 
 
-  //const parts = [...str]
-  // const arrToStr = noEmptyStrings.toString()
-
-
   /*
-  const arrToStr = noEmptyStrings.toString()
-  arrToStr[0].toUpperCase() + arrToStr.substring(1)
-   const bigChar = secondWord.
-
   const elem = noEmptyStrings.filter((word) => word.char[0])
     .map(), .filter(), .filter(Boolean)
-
-  const elem = join(parts, '')
     */
 
 }
