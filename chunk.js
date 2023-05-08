c14773b9ab57c14119033516df9d9411ded7afa9
Meowmir <disewji@gmail.com>
2023-05-08 09:41:26 +0200

    added js docs

--- chunk.js
+++ chunk.js
@@ -1,3 +1,10 @@
+/**
+ * Creates smaller arrays based on the second argument. If array is uneven, remaining elements will be chunked in the last array.
+ * @param {array} arr - The array to be chunked
+ * @param {int} int - The number of elements in each chunked array
+ * @returns
+ */
+
 export default function chunk(arr, int) {
 
   const sizeToUse = Math.floor(
