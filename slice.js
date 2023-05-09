34943636f16f2209f2b836015f9faf8321aaa290
Meowmir <disewji@gmail.com>
2023-05-09 17:33:09 +0200

    Added JS docs

--- slice.js
+++ slice.js
@@ -1,3 +1,11 @@
+/**
+ * Creates a subarray which starts at optional second argument and ends before the optional third argument.
+ * @param {array} array - The array to be sent into the function.
+ * @param {int} start - The index where to start the slice.
+ * @param {int} end - The index where to end the slice.
+ * @returns
+ */
+
 export default function slice(array, start, end) {
   const fixedStart = Number(start)
   // Number([[1], [2, 3]]) == NaN
