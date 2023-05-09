9389da37855a230a0d2cab00434dce9cb5653b69
Meowmir <disewji@gmail.com>
2023-05-09 14:19:27 +0200

    tried teathing end as 0, but not working

--- slice.js
+++ slice.js
@@ -1,7 +1,13 @@
+import { falsey } from './test/utils'
+
 export default function slice(array, start, end) {
   const fixedStart = Number(start)
   // Number([[1], [2, 3]]) == NaN
   const fixedEnd = isNaN(Number(end)) ? array.length : Number(end)
+
+  if (Number(end) === falsey) {
+    return 0
+  }
 
   if (Array.isArray(array)) {
     return array.slice(fixedStart, fixedEnd)
