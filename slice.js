4d4b0f848dae7f0a1f7bf46a5cd8135a3da72012
Meowmir <disewji@gmail.com>
2023-05-12 13:26:17 +0200

    changed name of parameter one, added return type to jsdoc

--- slice.js
+++ slice.js
@@ -1,27 +1,27 @@
 /**
  * Creates a subarray which starts at optional second argument and ends before the optional third argument.
- * @param {array} array - The array to be sent into the function.
+ * @param {array} toSlice - The array to be sent into the function.
  * @param {int} start - The index where to start the slice.
  * @param {int} end - The index where to end the slice.
- * @returns
+ * @returns {array}
  */
 
-export default function slice(array, start, end) {
+export default function slice(toSlice, start, end) {
   const fixedStart = Number(start)
   // Number([[1], [2, 3]]) == NaN
   let fixedEnd = Number(end)
 
   if (Array.isArray(end)) {
-    return array
+    return toSlice
   } else if (end !== undefined && isNaN(end)) {
     fixedEnd = 0
   } else if (isNaN(fixedEnd)) {
-    fixedEnd = array.length
+    fixedEnd = toSlice.length
   }
 
 
-  if (Array.isArray(array)) {
-    return array.slice(fixedStart, fixedEnd)
+  if (Array.isArray(toSlice)) {
+    return toSlice.slice(fixedStart, fixedEnd)
   }
 
   return []
