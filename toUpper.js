af7b291f9c0cc0f17bf7c7587c72d8d0a07dc91d
Meowmir <disewji@gmail.com>
2023-06-22 13:16:16 +0200

    Feature toUpper

---
+++ toUpper.js
@@ -0,0 +1,8 @@
+/**
+ @param {string} stringToConvert - The string to convert to upper case.
+ @returns {string} - Returns converted string.
+ */
+
+export default function toUpper(stringToConvert) {
+  return stringToConvert.toUpperCase()
+}
