31ef3ffc25f476b244ac0579479d12650b7c2d39
Nora <106168179+Meowmir@users.noreply.github.com>
2023-07-03 08:42:15 +0200

    Feature clamp (#12)
    * all but one test passing
    
    * passed the last test and added JS docs
    
    * removed unnecessary if statement

---
+++ clamp.js
@@ -0,0 +1,27 @@
+/**
+ * Clamps a number within the upper and lower boundaries.
+ * @param {number} numberToClamp - The number to clamp
+ * @param {number} minNum - The lower boundary
+ * @param {number} maxNum - The upper boundary
+ * @returns {number} - The clamped number
+ */
+
+export default function clamp(numberToClamp, minNum, maxNum = 0) {
+  if (isNaN(minNum) || isNaN(maxNum)) {
+    return 0
+  }
+
+  if (!maxNum) {
+    return Math.min(numberToClamp, minNum)
+  }
+
+  if (numberToClamp < minNum) {
+    return minNum
+  }
+
+  if (numberToClamp > maxNum) {
+    return maxNum
+  }
+
+  return numberToClamp
+}
