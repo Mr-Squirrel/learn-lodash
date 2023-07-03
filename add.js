f3970da3fd295b4cc391cf59938ad0d48ff5a36a
Nora <106168179+Meowmir@users.noreply.github.com>
2023-07-03 08:41:10 +0200

    Feature Add (#17)

---
+++ add.js
@@ -0,0 +1,9 @@
+/**
+ @param {value} valueOne
+ @param {value} valueTwo
+ @returns {value} - Returns the sum or merged result of the two values
+ */
+
+export default function add(valueOne, valueTwo) {
+  return valueOne + valueTwo
+}
