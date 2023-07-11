0e0fc8cf4770f69d1f30218a174df65b2f16e0ea
Meowmir <disewji@gmail.com>
2023-07-11 13:43:00 +0200

    boilplate

---
+++ omit.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {object} source - The array to drop from.
+ * @param {array} path - The number of elements to drop.
+ * @returns {object} - The array containing the remaining elements.
+ */
+
+export default function omit(source, path) {
+
+  return
+}
