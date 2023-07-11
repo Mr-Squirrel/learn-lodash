406e882ff14123c153ac2b4899c08faf8af070ab
Meowmir <disewji@gmail.com>
2023-07-11 11:52:20 +0200

    added new tests and passed

--- test/filter.test.js
+++ test/filter.test.js
@@ -3,9 +3,11 @@ import { isEven } from './utils.js'
 import filter from '../filter.js'
 
 describe('filter', () => {
-  const array = [1, 2, 3, 4]
+  const array = [1, 2, 3, 4, 'A']
 
   it('should return elements `predicate` returns truthy for', () => {
     assert.deepStrictEqual(filter(array, isEven), [2, 4])
+    assert.deepStrictEqual(filter(array, (i) => i > 3), [4])
+    assert.deepStrictEqual(filter(array, isNaN), ['A'])
   })
 })
