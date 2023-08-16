0553725f2069899bd46c3a6b0114110bba322dda
Meowmir <disewji@gmail.com>
2023-08-16 09:07:04 +0200

    bp

---
+++ delay.js
@@ -0,0 +1,10 @@
+/**
+ * @param {function} func - the function to execute
+ * @oaram {number} wait - the time to wait
+ * @oaram [args] args
+ * @returns {number} timer - the time to wait before executing function
+ */
+
+export default function delay(function(){}, wait, [args]) {
+  return
+}
