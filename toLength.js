4832e3f148b7005bf775a1ce8ac1ff68cb4e240f
Meowmir <disewji@gmail.com>
2023-08-18 11:00:27 +0200

    all tests passed

--- toLength.js
+++ toLength.js
@@ -2,13 +2,18 @@
  * @param {value} toBeConverted - the value to be converted
  * @returns {number} - the converted number
  */
+import {MAX_ARRAY_LENGTH} from './test/utils'
 
 export default function toLength(toBeConverted) {
 
-  const numb = Number(toBeConverted)
+  // let numb = Number(toBeConverted)
 
-  if (numb > 0) {
-    return Math.floor(numb)
+  if (toBeConverted > MAX_ARRAY_LENGTH) {
+    toBeConverted = MAX_ARRAY_LENGTH
   }
-  return Math.ceil(numb)
+
+  if (toBeConverted < 0 || toBeConverted === -0) {
+    return 0
+  }
+  return Math.floor(toBeConverted)
 }
