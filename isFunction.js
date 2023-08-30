7fd1054fa786c4cf7e8f12ee8ee70d6ef7607d15
Meowmir <disewji@gmail.com>
2023-08-30 11:00:40 +0200

    added docs

--- isFunction.js
+++ isFunction.js
@@ -1,6 +1,6 @@
 /**
- * @param
- * @returns
+ * @param {value} value - the value to check if it is a function
+ * @returns {boolean} - returns true if value is a function
  */
 
 export default function isFunction(value) {
