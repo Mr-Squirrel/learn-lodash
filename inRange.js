4a65b6d70e762cafe707cdb4246f473d63c1bd16
Nora <106168179+Meowmir@users.noreply.github.com>
2023-09-11 13:20:10 +0200

    passed all tests, added js docs (#13)

---
+++ inRange.js
@@ -0,0 +1,38 @@
+/**
+ * Checks if the given number is between the given start and end numbers. Including the start number but excluding the end.
+ * @param {number} numberToCheck - The number to be checked
+ * @param {number} startNumber - The start of the range, defaulted at 0
+ * @param {number} endNumber - The end of the range
+ * @returns {boolean}
+ */
+
+export default function inRange(
+  numberToCheck,
+  startNumber = 0,
+  endNumber) {
+
+  let start = startNumber
+  let end = endNumber
+
+  if (startNumber > endNumber || endNumber === undefined) {
+    start = endNumber
+    end = startNumber
+    startNumber = start
+    endNumber = end
+  }
+
+  if (!Number(startNumber)) {
+    startNumber = 0
+  }
+
+  if (!Number(endNumber)) {
+    Number(endNumber)
+    endNumber = 0
+  }
+
+  if (numberToCheck >= startNumber && numberToCheck < endNumber) {
+    return true
+  }
+
+  return false
+}
