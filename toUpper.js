26df742b57065bf6bb6fd40426825780e3e0c7c5
Meowmir <disewji@gmail.com>
2023-09-11 12:42:42 +0200

    added docs

--- toUpper.js
+++ toUpper.js
@@ -1,3 +1,8 @@
+/**
+ * @param {string} stringToConvert - the string to convert
+ * @returns {boolean|string} - returns the string in uppercase or false if value sent in is not a string
+ */
+
 export default function toUpper(stringToConvert) {
   if (typeof stringToConvert === 'string') {
     return stringToConvert.toUpperCase()
