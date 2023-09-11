478554977c4a96820b2a5de5d0de0f3dd4eaeb57
Meowmir <disewji@gmail.com>
2023-09-11 11:26:11 +0200

    passed all tests

---
+++ isArray.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param value - check if this is an array
+ * @returns {boolean} - returns true if value is array
+ */
+
+export default function isArray(value) {
+  return Array.isArray(value)
+}
