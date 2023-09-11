1f54761dd234a987a913288ef8c75928d27bb87d
Meowmir <disewji@gmail.com>
2023-09-11 14:04:59 +0200

    passed all tests

---
+++ test/subtract.test.js
@@ -0,0 +1,16 @@
+import assert from 'assert'
+import subtract from '../subtract.js'
+
+
+describe('subtract', () => {
+  it('should subtract two numbers', () => {
+    assert.strictEqual(subtract(6, 4), 2)
+    assert.strictEqual(subtract(-6, 4), -10)
+    assert.strictEqual(subtract(-6, -4), -2)
+  })
+
+  it('should coerce arguments to numbers', () => {
+    assert.strictEqual(subtract('6', '4'), 2)
+    assert.deepStrictEqual(subtract('x', 'y'), NaN)
+  })
+})
