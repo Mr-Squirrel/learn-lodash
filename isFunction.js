6ce995cdbe07297082efc0c54bac06cca60991f5
Nora <106168179+Meowmir@users.noreply.github.com>
2023-09-20 11:09:36 +0200

    Feature isFunction (#23)
    * passing all tests, but feels like it shouldnt
    
    * added docs
    
    * passing all tests for isFunction

---
+++ isFunction.js
@@ -0,0 +1,9 @@
+/**
+ * @param {value} value - the value to check if it is a function
+ * @returns {boolean} - returns true if value is a function
+ */
+
+export default function isFunction(value) {
+  return typeof value === typeof Function
+
+}
