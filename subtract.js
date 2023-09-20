c3a2bc3919e176c7730b5180d95f932939c403f0
Nora <106168179+Meowmir@users.noreply.github.com>
2023-09-20 11:10:33 +0200

    passed all tests (#29)

---
+++ subtract.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num1 - the first number in the subtraction
+ * @param {number} num2 - the second number in the subtraction
+ * @returns {number} - returns the difference
+ */
+
+export default function subtract(num1, num2) {
+  return Number(num1) - Number(num2)
+}
