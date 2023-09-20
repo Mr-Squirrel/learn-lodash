4679a3ba751c6fffa9369ce42339294c94987a4d
Nora <106168179+Meowmir@users.noreply.github.com>
2023-09-20 11:11:23 +0200

    passed all tests (#24)

---
+++ isNull.js
@@ -0,0 +1,8 @@
+/**
+ * @param {value} value - the value to check if null or not
+ * @returns {boolean} - returns true is value is null
+ */
+
+export default function isNull(value) {
+  return value === null
+}
